ad1278d05b422fc2bee7b4f798dbde8e127a6f42
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-04-25 19:18:09 -0300

    Atualizando main e exemplo

--- src/main.c
+++ src/main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "bison.tab.h"
 
 extern FILE *yyin;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  FILE *arquivo = fopen("../teste.txt", "r");
+  if (!arquivo) {
+    printf("Erro ao abrir arquivo %s\n", argv[1]);
+    return 1;
+  }
 
-    FILE *arquivo = fopen("/home/udesc/Documentos/trabalho-compiladores/teste.txt", "r");
-    if (!arquivo)
-    {
-        printf("Erro ao abrir arquivo %s\n", argv[1]);
-        return 1;
-    }
+  yyin = arquivo;
+  yyparse();
 
-    yyin = arquivo;
-    yyparse();
-
-    fclose(arquivo);
-    return 0;
+  fclose(arquivo);
+  return 0;
 }
