34b1e44b675286d7e160c870c04c8d9bc3823e86
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-05-31 14:39:07 -0300

    Reescrevendo a estrutura de dados

--- include/ast.h
+++ include/ast.h
@@ -7,7 +7,7 @@
 
 enum NoTipo { NoComando, NoExpressao, NoValor };
 
-enum Comando {
+enum ComandoTipo {
   ComandoIF,
   ComandoWhile,
   ComandoAtribuicao,
@@ -18,7 +18,7 @@ enum Comando {
   ComandoRead
 };
 
-enum Expressao { ExpressaoAritmetica, ExpressaoRelacional, ExpressaoLogica };
+enum Expressao { ExpressaoAritmetica, ExpressaoRelacional, ExpressaoLogica, ValorPuro, ExpressaoIgualdade };
 
 enum OperadorAr { OpSoma, OpSub, OpMult, OpDiv, OpPot };
 
@@ -28,35 +28,70 @@ enum OperadorLog { OpAnd, OpOr };
 
 enum OperadorIgualdade { OpEqual, OpNot };
 
-typedef union {
-  int inteiro;
-  float flutuante;
-  char *string;
-} Valor;
-
-struct ASTNode {
-  enum NoTipo tipo;
-  union {
-    enum Comando comando;
-    struct {
-      enum Expressao tipoExpressao;
-      union {
-        enum OperadorAr operadorAr;
-        enum OperadorRel operadorRel;
-        enum OperadorLog operadorLog;
-        enum OperadorIgualdade operadorIgualdade;
-      } operador;
-    } expressao;
-    Valor valor;
-  } tipoNo;
-
+struct NoExpressao {
+  enum Expressao tipo;
   union {
     struct {
-      struct ASTNode *esquerda;
-      struct ASTNode *direita;
-    } arvore;
-    cvector_vector_type(struct ASTNode *) vetor;
-  } instrucao;
+      enum OperadorAr operador;
+      struct NoExpressao *esquerda;
+      struct NoExpressao *direita;
+    } aritmetica;
+    struct {
+      enum OperadorRel operador;
+      struct NoExpressao *esquerda;
+      struct NoExpressao *direita;
+    } relacional;
+    struct {
+      enum OperadorLog operador;
+      struct NoExpressao *esquerda;
+      struct NoExpressao *direita;
+    } logica;
+    struct {
+      enum OperadorIgualdade operador;
+      struct NoExpressao *esquerda;
+      struct NoExpressao *direita;
+    } igualdade;
+    union {
+      int valorInt;
+      float valorFloat;
+      char *valorString;
+    } valor;
+  } expressao;
+};
+
+struct NoComando {
+  enum ComandoTipo tipo;
+  union {
+    struct {
+      struct NoExpressao *condicao;
+      cvector_vector_type(struct NoComando *) comandos;
+    } ifComando;
+    struct {
+      struct NoExpressao *condicao;
+      cvector_vector_type(struct NoComando *) comandos;
+    } whileComando;
+    struct {
+      char *id;
+      struct NoExpressao *expressao;
+    } atribuicao;
+    struct {
+      char *id;
+      struct NoExpressao *expressao;
+    } declaracao;
+    struct {
+      char *id;
+      cvector_vector_type(struct NoExpressao *) argumentos;
+    } chamadaFuncao;
+    struct {
+      struct NoExpressao *expressao;
+    } retorno;
+    struct {
+      struct NoExpressao *expressao;
+    } print;
+    struct {
+      char *id;
+    } read;
+  } comando;
 };
 
 #endif
