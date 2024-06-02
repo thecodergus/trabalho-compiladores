6cae3f67bbbf8fdafa83de13aff6d9f20f179305
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-06-02 13:18:51 -0300

    Separando o codigo

---
+++ include/astUtils.h
@@ -0,0 +1,10 @@
+#ifndef CCDA0A80_6C57_42F9_805F_FE00EACEF858
+#define CCDA0A80_6C57_42F9_805F_FE00EACEF858
+
+#include "ast.h"
+
+AST criar_programa(vector(struct AST) funcoes, vector(struct AST) blocoPrincipal);
+
+vector(AST) concat_vectors(vector(AST) a, vector(AST) b);
+
+#endif /* CCDA0A80_6C57_42F9_805F_FE00EACEF858 */
--- src/astUtils.c
+++ src/astUtils.c
@@ -1,6 +1,6 @@
 #include "ast.h"
 
-AST criar_programa(vector(struct AST *) funcoes, vector(struct AST *) blocoPrincipal) {
+AST criar_programa(vector(struct AST) funcoes, vector(struct AST) blocoPrincipal) {
   AST programa;
   programa.tipo = Vetor;
   programa.u.filhos = concat_vectors(funcoes, blocoPrincipal);
@@ -8,18 +8,18 @@ AST criar_programa(vector(struct AST *) funcoes, vector(struct AST *) blocoPrinc
   return programa;
 }
 
-vector(AST *) concat_vectors(vector(AST *) a, vector(AST *) b) {
-  vector(AST *) result = NULL;
+vector(AST) concat_vectors(vector(AST) a, vector(AST) b) {
+  vector(AST) result = NULL;
 
   if (a) {
     for (AST *it = cvector_begin(a); it != cvector_end(a); ++it) {
-      cvector_push_back(result, it);
+      cvector_push_back(result, *it);
     }
   }
 
   if (b) {
     for (AST *it = cvector_begin(b); it != cvector_end(b); ++it) {
-      cvector_push_back(result, it);
+      cvector_push_back(result, *it);
     }
   }
 
