5f279dd5e5b01999fefbd5ca7563ae4c7ac6b1a9
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-06-02 20:21:50 -0300

    Adicionando tradução de operadores

--- include/utils.h
+++ include/utils.h
@@ -15,4 +15,10 @@ void imprimir_token(Token token, int nivel);
 
 void imprimir_traco(int nivel);
 
+char* operador_logico_str(enum LogicalOperators op);
+
+char* operador_aritmetico_str(enum ArithmeticOperators op);
+
+char* operador_relacional_str(enum RelationsOperators op);
+
 #endif /* CCDA0A80_6C57_42F9_805F_FE00EACEF858 */
--- src/utils.c
+++ src/utils.c
@@ -8,20 +8,20 @@ void imprimir_arvore(AST* raiz, int nivel) {
   switch (raiz->tipo) {
     case Arvore:
       printf("Arvore:\n");
-      imprimir_token(raiz->token, nivel);
-      imprimir_arvore(raiz->u.arvore.left, nivel + 1);
-      imprimir_arvore(raiz->u.arvore.right, nivel + 1);
+      imprimir_token(raiz->token, nivel + 1);
+      imprimir_arvore(raiz->u.arvore.left, nivel + 2);
+      imprimir_arvore(raiz->u.arvore.right, nivel + 2);
       break;
     case Vetor:
       printf("Vetor:\n");
       imprimir_token(raiz->token, nivel);
       for (AST** i = cvector_begin(raiz->u.filhos); i != cvector_end(raiz->u.filhos); i++) {
-        imprimir_arvore(*i, nivel + 1);
+        imprimir_arvore(*i, nivel + 2);
       }
       break;
     case Folha:
       printf("Folha: \n");
-      imprimir_token(raiz->token, nivel + 1);
+      imprimir_token(raiz->token, nivel + 2);
       break;
   }
 }
@@ -135,13 +135,13 @@ void imprimir_token(Token token, int nivel) {
       printf("Chamada de funcao\n");
       break;
     case ExpressionArithmetic:
-      printf("Expressao Aritimetica, Operator: %d\n", token.u.arithmeticExpression.operator);
+      printf("Expressao Aritimetica, Operator: %d\n", operador_aritmetico_str(token.u.arithmeticExpression.operator));
       break;
     case ExpressionRelational:
-      printf("Expressao Relacional, Operator: %d\n", token.u.relationalExpression.operator);
+      printf("Expressao Relacional, Operator: %d\n", operador_relacional_str(token.u.relationalExpression.operator));
       break;
     case ExpressionLogical:
-      printf("Expressao Logica, Operator: %d\n", token.u.logicalExpression.operator);
+      printf("Expressao Logica, Operator: %d\n", operador_logico_str(token.u.logicalExpression.operator));
       break;
     case Variable:
       printf("Variavel\n");
@@ -180,4 +180,53 @@ void imprimir_token(Token token, int nivel) {
 
 void imprimir_traco(int nivel) {
   for (int i = 0; i < nivel; i++) printf("-");
+}
+
+char* operador_logico_str(enum LogicalOperators op) {
+  switch (op) {
+    case E:
+      return "E(&&)";
+    case Ou:
+      return "Ou(||)";
+    case Nao:
+      return "Nao(!)";
+    default:
+      return "Unknown";
+  }
+}
+
+char* operador_aritmetico_str(enum ArithmeticOperators op) {
+  switch (op) {
+    case Soma:
+      return "Soma(+)";
+    case Subtracao:
+      return "Subtracao(-)";
+    case Multiplicacao:
+      return "Multiplicacao(*)";
+    case Divisao:
+      return "Divisao(/)";
+    case Potencia:
+      return "Potencia(^)";
+    default:
+      return "Unknown";
+  }
+}
+
+char* operador_relacional_str(enum RelationsOperators op) {
+  switch (op) {
+    case Igual:
+      return "Igual(==)";
+    case Diferente:
+      return "Diferente(!=)";
+    case Menor:
+      return "Menor(<)";
+    case MenorIgual:
+      return "MenorIgual(<=)";
+    case Maior:
+      return "Maior(>)";
+    case MaiorIgual:
+      return "MaiorIgual(>=)";
+    default:
+      return "Unknown";
+  }
 }
