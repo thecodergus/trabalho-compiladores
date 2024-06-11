77242191f2bb25a31904f9e33de8e491cab936a0
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-06-11 17:11:58 -0300

    Adicionando codigo para exibir mensagens coloridas

---
+++ src/mensagens.c
@@ -0,0 +1,13 @@
+#include "mensagens.h"
+
+void exibir_warning(const char *msg) {
+  if (msg) {
+    printf(YELLOW "[AVISO]:\t%s\t[AVISO]\n" RESET, msg);
+  }
+}
+
+void exibir_erro(const char *msg) {
+  if (msg) {
+    printf(RED "[ERRO]:\t%s\t[ERRO]\n" RESET, msg);
+  }
+}
