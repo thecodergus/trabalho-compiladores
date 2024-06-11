18b3910265a701556ca959bd968ee5cacc1b98a5
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-06-11 17:19:53 -0300

    Setando o que quero para mensagens

--- src/mensagens.c
+++ src/mensagens.c
@@ -2,12 +2,12 @@
 
 void exibir_warning(const char *msg) {
   if (msg) {
-    printf(YELLOW "[AVISO]:\t%s\t[\AVISO]\n" RESET, msg);
+    printf(YELLOW "[AVISO]\t%s\t[AVISO]\n" RESET, msg);
   }
 }
 
 void exibir_erro(const char *msg) {
   if (msg) {
-    printf(RED "[ERRO]:\t%s\t[\ERRO]\n" RESET, msg);
+    printf(RED "[ERRO]\t%s\t[ERRO]\n" RESET, msg);
   }
 }
