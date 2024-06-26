d59ca948bbf6640944cf6cf88a7b1fc2e5beeff2
PCdaFacul <gustavo.michels.de.camargo@gmail.com>
2024-06-26 16:43:44 -0300

    VOu terminar em casa a tabela de simbolos

--- include/tabela_simbolos.h
+++ include/tabela_simbolos.h
@@ -5,16 +5,21 @@
 #include "tipos.h"
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 enum TipoItem { Fn, Var };
 
 typedef struct {
   enum TipoItem tipo_item;
-  const char *escopo;
-  const char *id;
+  char *escopo;
+  char *id;
   enum TipoDado tipo_declarado;
 } Linha;
 
 extern cvector_vector_type(Linha) tabela_simbolos;
+
+void adicionar_funcao(enum TipoDado tipo, const char *id);
+void adicionar_variavel(const char *fn_id, enum TipoDado tipo, const char *id);
+
 
 #endif
--- src/tabela_simbolos.c
+++ src/tabela_simbolos.c
@@ -1,3 +1,22 @@
 #include "tabela_simbolos.h"
 
 cvector_vector_type(Linha) tabela_simbolos = NULL;
+
+void adicionar_funcao(enum TipoDado tipo, const char *id) {
+  Linha aux;
+  aux.escopo = NULL;
+  aux.id = strdup(id);
+  aux.tipo_item = Fn;
+  aux.tipo_declarado = tipo;
+
+  cvector_push_back(tabela_simbolos, aux);
+}
+
+void adicionar_variavel(const char *fn_id, enum TipoDado tipo, const char *id) {
+  Linha aux;
+  aux.escopo = strdup(fn_id);
+  aux.tipo_declarado = tipo;
+  aux.id = strdup(id);
+
+  cvector_push_back(tabela_simbolos, aux);
+}
