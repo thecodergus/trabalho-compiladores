1eb9f01c3cd061b74547a6c5a5f8c0f922783a05
PCdaFacul <gustavo.michels.de.camargo@gmail.com>
2024-06-27 14:02:06 -0300

    Criando tabela de simbolos

--- include/tabela_simbolos.h
+++ include/tabela_simbolos.h
@@ -9,17 +9,21 @@
 
 enum TipoItem { Fn, Var };
 
-typedef struct {
+typedef struct Linha {
   enum TipoItem tipo_item;
   char *escopo;
   char *id;
   enum TipoDado tipo_declarado;
 } Linha;
 
-extern cvector_vector_type(Linha) tabela_simbolos;
+extern cvector_vector_type(struct Linha) tabela_simbolos;
 
 void adicionar_funcao(enum TipoDado tipo, const char *id);
 void adicionar_variavel(const char *fn_id, enum TipoDado tipo, const char *id);
+bool id_sendo_usado_por_variavel(const char *fn_id, const char *id);
+bool id_sendo_usado_por_funcao(const char *id);
+enum TipoDado get_tipo_dado_id_variavel(const char *fn_id, const char *id);
+enum TipoDado get_tipo_dado_id_funcao(const char *id);
 
 
 #endif
--- src/tabela_simbolos.c
+++ src/tabela_simbolos.c
@@ -1,6 +1,6 @@
 #include "tabela_simbolos.h"
 
-cvector_vector_type(Linha) tabela_simbolos = NULL;
+cvector_vector_type(struct Linha) tabela_simbolos = NULL;
 
 void adicionar_funcao(enum TipoDado tipo, const char *id) {
   Linha aux;
@@ -19,4 +19,50 @@ void adicionar_variavel(const char *fn_id, enum TipoDado tipo, const char *id) {
   aux.id = strdup(id);
 
   cvector_push_back(tabela_simbolos, aux);
+}
+
+bool id_sendo_usado_por_variavel(const char *fn_id, const char *id) {
+  for (Linha *l = cvector_begin(tabela_simbolos);
+       l != cvector_end(tabela_simbolos); l++) {
+    if (strcmp(l->id, id) == 0 && l->tipo_item == Var &&
+        strcmp(l->escopo, fn_id) == 0) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool id_sendo_usado_por_funcao(const char *id) {
+  for (Linha *l = cvector_begin(tabela_simbolos);
+       l != cvector_end(tabela_simbolos); l++) {
+    if (strcmp(l->id, id) == 0 && l->tipo_item == Fn) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+enum TipoDado get_tipo_dado_id_variavel(const char *fn_id, const char *id) {
+  for (Linha *l = cvector_begin(tabela_simbolos);
+       l != cvector_end(tabela_simbolos); l++) {
+    if (strcmps(l->id, id) == 0 && l->tipo_item == Var &&
+        strcmp(l->escopo, fn_id) == 0) {
+      return l->tipo_declarado;
+    }
+  }
+
+  return SemTipo;
+}
+
+enum TipoDado get_tipo_dado_id_funcao(const char *id) {
+  for (Linha *l = cvector_begin(tabela_simbolos);
+       l != cvector_end(tabela_simbolos); l++) {
+    if (strcmp(l->id, id) == 0 && l->tipo_item == Fn) {
+      return l->tipo_declarado;
+    }
+  }
+
+  return SemTipo;
 }
